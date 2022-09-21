1e5066ea1c8cd32f7e8a53e60c75675b7e7a663e
maxim19780306 <rm83788@gmail.com>
2022-09-21 14:31:07 +0300

    Initial commit

---
+++ PaymentApp/src/main/java/com/example/PaymentApp/model/Payment.java
@@ -0,0 +1,11 @@
+package com.example.PaymentApp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Payment {
+    private String id;
+    private double amount;
+}
